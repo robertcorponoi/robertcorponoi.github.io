e442f7bedacdb394809f4274ad661353bc18aee3
Robert Corponoi <robertcorponoi@gmail.com>
2021-10-30 09:48:26 -0700

    Added utterances comments

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import Seo from "../Seo";
 import Layout from "../Layout";
@@ -26,10 +26,43 @@ interface PostProps {
  * @param {PostProps} props
  */
 const Post = ({ title, created, description, tags, content, location }: PostProps) => {
+    /** A ref to the article content, used to position the comments widget. */
+    const articleRef = useRef<HTMLDivElement | null>(null);
+
+    /**
+     * When the component mounts we add the utterances script to the body and 
+     * remove it on unmount.
+     */
+    useEffect(() => {
+        if (articleRef && articleRef.current) {
+            const script = document.createElement("script");
+
+            script.setAttribute("src", "https://utteranc.es/client.js");
+            script.setAttribute("repo", "robertcorponoi/robertcorponoi.github.io");
+            script.setAttribute("issue-term", "pathname");
+            script.setAttribute("theme", "github-dark");
+            script.setAttribute("crossorigin", "anonymous");
+            script.setAttribute("async", "true");
+
+            console.log(script);
+
+            articleRef.current.appendChild(script);
+
+            return () => {
+                articleRef.current.removeChild(script);
+            };
+        }
+    }, [articleRef]);
+
     return (
         <Layout location={location}>
             <Seo title={title} description={description} />
-            <article itemScope itemType="http://schema.org/Article" className="mt-8">
+            <article
+                ref={articleRef}
+                itemScope
+                itemType="http://schema.org/Article"
+                className="mt-8"
+            >
                 <PostHeader title={title} date={created} />
                 <PostTags tags={tags} />
                 <section
