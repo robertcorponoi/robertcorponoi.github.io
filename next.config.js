2d46229e36d2e4c48040afabf40f0d1cc80b873c
Robert Corponoi <robertcorponoi@gmail.com>
2023-03-06 09:20:27 -0500

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/robertcorponoi.github.io",
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
     reactStrictMode: true,
 };
 
