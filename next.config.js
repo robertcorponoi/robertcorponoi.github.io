f0879c81b233e214501e7ef14910cf0b56f3332a
Robert Corponoi <robertcorponoi@gmail.com>
2023-03-07 20:37:22 -0500

    Updated base path in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/robertcorponoi.github.io",
+    basePath: "/robertcorponoi.com",
     output: "export",
     images: {
         unoptimized: true,
